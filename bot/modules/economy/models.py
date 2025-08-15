dcd7cdb1af22c5a9422e71be2237f5fe264163f1
HyScript7 <hyscript7@seznam.cz>
2025-08-15 16:39:28 +0200

    Feat: Roulette
    Fix: Star claim not working due to tz-aware and naive datetimes being mixed up

--- bot/modules/economy/models.py
+++ bot/modules/economy/models.py
@@ -9,3 +9,6 @@ class EconomyModel(SQLModel, table=True):
     last_claim: datetime = Field(
         default_factory=lambda: datetime.fromtimestamp(1, tz=timezone.utc)
     )
+    @property
+    def utc_last_claim(self):
+        return self.last_claim.astimezone(tz=timezone.utc)
